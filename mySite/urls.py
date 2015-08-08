1ea943cbbb714390e7d18bea92afa625bb90160b
A. Drake <a.l.drake713@gmail.com>
2015-08-08 14:35:32 -0400

    Add basic views and routing

--- mySite/urls.py
+++ mySite/urls.py
@@ -6,5 +6,15 @@ urlpatterns = patterns('',
     # url(r'^$', 'mySite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # url function takes two argumnets: a regex, and a view, 
+    # and two optionals: kwargs and name
+
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# above, note there is no 'end of line' regex marker.
+# Django takes whatever is left after the match and sends it as a parameter
+# to the include() URLconf for the rest of the processing.
+
+#functionality intended to provide 'plug and play' easy addition.
---
+++ polls/urls.py
@@ -0,0 +1,27 @@
+# creating the URL mapping (routes)
+
+from django.conf.urls import patterns, url
+from polls import views
+
+urlpatterns = patterns('', 
+      # ex: /polls/
+      url(r'^$', views.index, name='index'),
+      # ex: /polls/4
+      url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+      # ex: /polls/4/results
+      url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+      # ex: /polls/3/vote/
+      url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+      )
+
+# ====== QUESTIONS =======
+# HOW TO 
+# ... do error handling?
+
+# using capture patterns with regex, and using the ?P<parameter_name>
+# defines what that parameter should be called when it is sent to the function
+# d+ is of course the regex pattern that will match and be captured.
+
+# The only thing that Django wants is either:
+#   1) an HttpResponse
+#   2) an exception
